d7986c470d4e7747a469a4ae13928b990944b50b
Gorz <GorzD3v@gmail.com>
2021-08-25 18:34:17 -0300

    Diseño de interfaz

---
+++ script.js
@@ -0,0 +1,21 @@
+let miLibreria = [];
+
+const btn = document.querySelector(".btn");
+
+function libro (titulo , autor , paginas , estado) {
+
+    titulo=this.titulo;
+    autor=this.autor;
+    paginas=this.paginas;
+    estado=this.estado;
+
+}
+
+function añadirLibro(){
+
+    
+
+}
+
+
+btn.addEventListener('click' , añadirLibro);
